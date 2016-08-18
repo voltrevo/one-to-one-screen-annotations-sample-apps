43e6e83dc1f4ce4aee55b909f3bbf89515ffbd80
Esteban Cordero <esteban@agilityfeat.com>
2016-08-17 23:34:52 -0600

    apply the standard for id name (#78)

--- android/ScreensharingAccPackKit/screensharing-acc-pack-kit/src/main/java/com/tokbox/android/accpack/screensharing/config/OpenTokConfig.java
+++ android/ScreensharingAccPackKit/screensharing-acc-pack-kit/src/main/java/com/tokbox/android/accpack/screensharing/config/OpenTokConfig.java
@@ -4,7 +4,7 @@ public class OpenTokConfig {
 
     // For internal use only. Please do not modify or remove this code.
     public static final String LOG_CLIENT_VERSION = "android-vsol-1.0.0";
-    public static final String LOG_COMPONENTID = "screensharingAccPack";
+    public static final String LOG_COMPONENTID = "screenSharingAccPack";
     public static final String LOG_ACTION_INITIALIZE = "Init";
     public static final String LOG_ACTION_DESTROY = "Destroy";
     public static final String LOG_ACTION_START = "Start";
