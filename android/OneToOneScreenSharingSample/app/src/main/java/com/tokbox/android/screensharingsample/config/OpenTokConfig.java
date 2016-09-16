c99bf3100d9f907f775230cdaaecf4ee14a5910a
marina serrano <marinas@tokbox.com>
2016-09-16 11:30:50 +0200

    Removing android credentials (#99)

--- android/OneToOneScreenSharingSample/app/src/main/java/com/tokbox/android/screensharingsample/config/OpenTokConfig.java
+++ android/OneToOneScreenSharingSample/app/src/main/java/com/tokbox/android/screensharingsample/config/OpenTokConfig.java
@@ -4,11 +4,11 @@ public class OpenTokConfig {
 
     // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
     // ***                      https://dashboard.tokbox.com/projects                           ***
-    public static final String SESSION_ID = "1_MX40NTYyMTE3Mn5-MTQ2ODQwNzgxNjY4NX5CaFRLV2J5RDZUUXBZek1xTVVoZEtyQ25-fg";
+    public static final String SESSION_ID = "";
     // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NTYyMTE3MiZzZGtfdmVyc2lvbj10YnBocC12MC45MS4yMDExLTA3LTA1JnNpZz05N2IzNjFmNDM3YjZlMDUxOWU3NDVjNjQwMDg3OTUzMjNmZDcxMDFkOnNlc3Npb25faWQ9MV9NWDQwTlRZeU1URTNNbjUtTVRRMk9EUXdOemd4TmpZNE5YNUNhRlJMVjJKNVJEWlVVWEJaZWsxeFRWVm9aRXR5UTI1LWZnJmNyZWF0ZV90aW1lPTE0NzIwNDgxNzgmcm9sZT1tb2RlcmF0b3Imbm9uY2U9MTQ3MjA0ODE3OC4yMjI4MTczNzc4NTMzNyZleHBpcmVfdGltZT0xNDc0NjQwMTc4";
+    public static final String TOKEN = "";
     // Replace with your OpenTok API key
-    public static final String API_KEY = "45621172";
+    public static final String API_KEY = "";
 
     // Subscribe to a stream published by this client. Set to false to subscribe
     // to other clients' streams only.
